ec871dbd9cf42746762a80f586be8cee8d817081
Reria <42136407+Reria@users.noreply.github.com>
2024-03-29 17:15:38 +0300

    Done task Day5

---
+++ Day5/index.js
@@ -0,0 +1,11 @@
+(function() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const message = document.querySelector('.message');
+    const content = document.querySelector('.content');
+    content.textContent = `"${message.value}" this is your message`;
+    message.value = "";
+  })
+})()
